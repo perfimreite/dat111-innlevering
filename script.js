73fc63eb9a58a3b11e66a5e846bd2693cf56a264
Per Fimreite <per.fimreite@gmail.com>
2025-10-30 11:39:09 +0100

    passed all validators

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ newsletterForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const name = document.getElementById("name").value;
+    const phoneNumber = document.getElementById("phone-number").value;
     const email = document.getElementById("email").value;
 
-    subscribers.push({ name: name, email: email });
+    subscribers.push({ name: name, phoneNumber: phoneNumber, email: email });
     alert("Gratulerar! Du abonnerar nå på vårt nyheitsbrev.");
+
+    newsletterForm.reset();
 });
